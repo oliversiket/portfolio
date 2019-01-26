23ffc75b22e690b5e21e3ffe388e306cc818b477
holyoliver <holyoliver@gmail.com>
2019-01-26 20:23:26 +0000

    mobile test

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,3 +13,26 @@ $(document).scroll(function(){
 
   }
 });
+
+/*
+$(document).ready(function() {
+
+    $(window).scroll( function(){
+
+        $('.projecthead').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1500);
+
+            }
+
+        });
+
+    });
+
+});
+*/
